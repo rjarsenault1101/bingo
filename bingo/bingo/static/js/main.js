c1eb01c792f379ee806b01167500f25de95e98fb
rjarsenault1101 <rjarsenault1101@gmail.com>
2020-07-01 18:08:10 -0400

    can now click cells!

--- bingo/bingo/static/js/main.js
+++ bingo/bingo/static/js/main.js
@@ -1,4 +1,26 @@
 
 (function ($) {
-	"use strict";
+	$("td.free").click(function(e) {   
+		console.log("FREE!"); 
+		e.preventDefault();
+		var color = $(this).css("background-color");
+		console.log(color);
+		if(color == 'rgb(92, 135, 39)'){
+			$(this).css("background-color","#aaaa00");
+		} else {
+			$(this).css("background-color","#5c8727");
+		}
+		e.stopImmediatePropagation();
+	  });
+	$("td").click(function(e) {
+		console.log("TD!");
+		e.preventDefault();
+		var color = $(this).css("background-color");
+		console.log(color);
+		if(color == 'rgb(255, 255, 255)'){
+			$(this).css("background-color","#aaaa00");
+		} else {
+			$(this).css("background-color","#fff");
+		}
+	})
 })(jQuery);
