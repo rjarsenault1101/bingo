dbd09e1bb9830758f8610970c8c2ac040cffae67
rjarsenault1101 <rjarsenault1101@gmail.com>
2020-07-05 08:02:44 -0400

    Added chat demo

--- bingo/caller/views.py
+++ bingo/caller/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from init.models import Callable
 from .models import CalledNumber
-
+import random
 # Create your views here.
 def caller(request): 
     return render(request, 'caller.html')
 
 def new_number(request):
-    # put a new number that hasn't been called into the database
     # Get what's been called, get what can be called
     # Subtract what's been called from what can be called
     # Pick a random element from that remaining list
-    callable_items = Callable.objects.values('value').exclude(value__in=CalledNumber.objects.all().values_list('number', flat=True)).all().values_list('value', flat=True)
+    callable_items = list(Callable.objects.values('value').exclude(value__in=CalledNumber.objects.all().values_list('number', flat=True)).values_list('value', flat=True))
     print(callable_items)
+    new_number = CalledNumber(number=int(random.choice(callable_items)))
+    new_number.save()
+    
     return render(request, 'caller.html')
 
 def clear_calls(request):
     # This goes and deletes all from the called database
-    pass
+    CalledNumber.objects.all().delete()
+    return render(request, 'caller.html')
