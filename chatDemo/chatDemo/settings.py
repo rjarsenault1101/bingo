2fa0e192b1b4dbb1e9b4906cc5fb6d7b08c72a14
RJ <rjarsenault1101@gmail.com>
2020-07-05 21:12:31 -0400

    finished chat demo

--- chatDemo/chatDemo/settings.py
+++ chatDemo/chatDemo/settings.py
@@ -46,7 +46,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6380)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
