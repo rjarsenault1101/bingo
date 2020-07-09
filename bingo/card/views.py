89e6e5810a5dc278e7842b78ac41ac7a4fe5f094
RJ <rjarsenault1101@gmail.com>
2020-07-09 16:09:16 -0400

    Generating cards as bingo do

--- bingo/card/views.py
+++ bingo/card/views.py
@@ -9,19 +9,42 @@ def index(request):
         }
     else:
         context = {
-            'numbers': generateNumbers()
+            'numbers': generate_card()
         }
     request.session['numbers'] = context['numbers']
     return render(request, 'index.html', context)
 
-def generateNumbers(): 
-    list= random.sample(range(1, 100), 25)
+def generate_card(): 
+    b = [i for i in range(1,16)]
+    i = [i for i in range(16,31)]
+    n = [i for i in range(31,46)]
+    g = [i for i in range(46,61)]
+    o = [i for i in range(61,76)]
+    list=[]
+    for value in range(25): 
+        if value % 5 == 0: 
+            list.append(get_number(b))
+        if value % 5 == 1: 
+            list.append(get_number(i))
+        if value % 5 == 2: 
+            list.append(get_number(n))
+        if value % 5 == 3: 
+            list.append(get_number(g))
+        if value % 5 == 4: 
+            list.append(get_number(o))
+        print(list[value])
     list[12]="FREE"
     return list
 
+def get_number(number_list):
+    number = random.choice(number_list)
+    number_list.remove(number)
+    return number
+
+
 def new_card(request):
     context = {
-            'numbers': generateNumbers()
+            'numbers': generate_card()
     }
     request.session['numbers'] = context['numbers']
     return redirect('index')
