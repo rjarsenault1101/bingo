93ae952dcddda1c34a79d265d88de481f48f70c3
RJ <rjarsenault1101@gmail.com>
2020-07-16 19:52:38 -0400

    Adding prints

--- init/views.py
+++ init/views.py
@@ -3,6 +3,6 @@ from .models import Callable
 from django.http import JsonResponse
 # Create your views here.
 def numbers(request):
-    callable = list(Callable.objects.all().values_list('value', flat=True)).sort()
+    callable = list(Callable.objects.all().order_by('value').values_list('value', flat=True))
     print(callable)
     return JsonResponse(callable, safe=False)
