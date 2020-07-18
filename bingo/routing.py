c0b7b5054dcc47c0a17d95d70d772ae2330ad4c1
RJ <rjarsenault1101@gmail.com>
2020-07-18 15:27:14 -0400

    Changed routing

--- bingo/routing.py
+++ bingo/routing.py
@@ -1,11 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import caller.routing
+from caller.consumers import BingoConsumer, CallConsumer
+from django.conf.urls import url
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            caller.routing.websocket_urlpatterns
-        )
+     "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^ws/bingo/$", BingoConsumer),
+            url(r"^ws/call/$", CallConsumer),
+        ])
     ),
 })
