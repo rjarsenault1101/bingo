d129698ae1ca92d8d0a24037ea7bccf98063f292
RJ <rjarsenault1101@gmail.com>
2020-07-22 17:57:07 -0400

    adding teams to init

--- init/models.py
+++ init/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Callable(models.Model):
     value = models.CharField(max_length=50)
+
+class Team(models.Model):
+    value = models.CharField(max_length=50)
--- init/views.py
+++ init/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Callable
 from django.http import JsonResponse
+from django.contrib.admin.views.decorators import staff_member_required
 
+@staff_member_required
 def index(request):
+    if request.method == 'POST': 
+        teams = request.POST['teamsbox'];
+        print(teams)
+        return render(request, 'caller.html')
     return render(request, 'init.html')
 
 # Create your views here.
