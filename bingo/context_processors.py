202cfdf942ccc952ebeee6b271f4733c0632cdcb
RJ <rjarsenault1101@gmail.com>
2020-07-23 18:24:43 -0400

    Adding default context

---
+++ bingo/context_processors.py
@@ -0,0 +1,8 @@
+from init.models import Info
+
+def add_variable_to_context(request):
+    info = Info.objects.all().first()
+    return {
+        'groupName': info['group_name'],
+        'cardName': info['cardName']
+    }
