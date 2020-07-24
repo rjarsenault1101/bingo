37a83c12b266bf5c19a94d7db614ac3fe98e1a4a
RJ <rjarsenault1101@gmail.com>
2020-07-23 20:15:15 -0400

    Saving more info to db

--- bingo/context_processors.py
+++ bingo/context_processors.py
@@ -3,6 +3,6 @@ from init.models import Info
 def add_variable_to_context(request):
     info = Info.objects.all().first()
     return {
-        'groupName': info['group_name'],
-        'cardName': info['cardName']
+        'groupName': info.group_name,
+        'cardName': info.card_name
     }
--- init/views.py
+++ init/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from .models import Callable, Team
+from .models import Callable, Team, Info
 from django.contrib.admin.views.decorators import staff_member_required
 
 @staff_member_required
@@ -15,14 +15,18 @@ def index(request):
         'values': callable_values,
         'teams': teams, 
     }
+
     if request.method == 'POST':
         Team.objects.all().delete()
         Callable.objects.all().delete()
+        Info.objects.all().delete()
+
         if request.POST.get('teams'):
             teams = request.POST['teamsbox'].split(",")
             for team in teams:
                 db_team = Team(team=team.strip())
                 db_team.save()
+
         if request.POST['bingo'] == 'custom':
             callable_values = request.POST['customvalues'].split(',')
             for value in callable_values:
@@ -32,6 +36,17 @@ def index(request):
             for value in range(1, 76):
                 Callable(value=value).save()
 
+        if 'name' in request.POST:
+            group_name = request.POST['name']
+
+        if 'cardheader' in request.POST:
+            card_header = request.POST['cardheader']
+
+        Info(
+            card_name=card_header if card_header else "bingo",
+            group_name=group_name if group_name else "Bingo!"
+        ).save()
+        
         return redirect('index')
     return render(request, 'init.html', context)
 
