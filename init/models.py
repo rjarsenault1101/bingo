dce1013f8b8c695b26d6958da8b02d4b0556f523
RJ <rjarsenault1101@gmail.com>
2020-08-02 09:25:16 -0400

    added logged in count

--- init/models.py
+++ init/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Callable(models.Model):
     value = models.CharField(max_length=50)
+
+class Info(models.Model):
+    logged_in = models.IntegerField()
