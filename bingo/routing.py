d13f8168133d5644bf2c3055a1d2676b4642a23e
RJ <rjarsenault1101@gmail.com>
2020-08-04 19:30:32 -0400

    Fixed routing

--- bingo/routing.py
+++ bingo/routing.py
@@ -1,14 +1,15 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from caller.consumers import BingoConsumer, CallConsumer
+from bingoAuth.consumers import ActiveConsumer
 from django.conf.urls import url
 
 application = ProtocolTypeRouter({
-     "websocket": AuthMiddlewareStack(
+    "websocket": AuthMiddlewareStack(
         URLRouter([
             url(r"^ws/bingo/$", BingoConsumer),
             url(r"^ws/call/$", CallConsumer),
-            url(r"^ws/active/$", )
+            url(r"^ws/active/$", ActiveConsumer),
         ])
     ),
 })
