ebcacadddbb13b5660af92efdf04c3b4e2b16d27
RJ <rjarsenault1101@gmail.com>
2020-08-04 20:16:08 -0400

    Altered model

--- init/models.py
+++ init/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Callable(models.Model):
     value = models.CharField(max_length=50)
+
 
 class Info(models.Model):
     logged_in = models.IntegerField()
 
+
 class WasActive(models.Model):
-    username = models.CharField(max_length=50)
-    team = models.CharField(max_length=50)
-    duration = models.IntegerField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    duration = models.IntegerField(default=0)
+    bingos = models.IntegerField(default=0)
